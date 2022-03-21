233c18c70c7c546f779210f6d4287503fc6247ff
marekvecerka <vec38035@spseol.cz>
2022-03-21 09:35:21 +0100

    škola

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python3
 
+import pylab as pl
+from pylab import linspace, pi, plot, sin, cos, show
 
-with open("soubor.txt", "r") as f:
-    pass
+
+def fceSoubor():
+    cesta = souborVar.get()
+    f = open(cesta,'r')
+    x=[]
+    y=[]
+    while 1:
+        radek=f.readline()
+        if radek=='':
+            break
+        cisla=radek.split()
+        x.append( float(cisla[0]) )
+        y.append( float(cisla[1]) )
+        f.close()
+        pl.figure()
+        pl.plot(x,y)
+        pl.xlabel(osaxVar.get())
+        pl.ylabel(osayVar.get())
+        pl.grid(True)
+        pl.show()
+    
